71e22ef16753e58fafe50506ed0a5ab3ee2a305a
Zaafira Sekar Safitri <zaafirasf@gmail.com>
2024-02-23 12:55:07 +0000

    testing

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -1,60 +1,37 @@
 {{
-
     config (
         materialized='table'
     )
 }}
 
-with orders as (
+with customers as (
+    select * from {{ref ('stg_customers') }}
+),
 
-  select 
-      id as order_id,
-      user_id as customer_id,
-      order_date,
-      status
-
-  from `dbt-tutorial.jaffle_shop.orders`
-
+orders as (
+    select * from {{ref ('stg_orders')}}
 ),
 
 customer_orders as (
-
-  select 
-      customer_id, 
+  select
+      customer_id,
       min(order_date) as first_order_date,
       max(order_date) as most_recent_order_date,
       count(order_id) as number_of_orders
-  
   from orders
-
   group by 1
-), 
-
-customers as (
-
-  select
-      id as customer_id, 
-      first_name, 
-      last_name
-    
-  from `dbt-tutorial.jaffle_shop.customers`
-
 ),
 
 final as (
-
   select
       cu.customer_id,
-      cu.first_name, 
-      cu.last_name, 
-      co.first_order_date, 
+      cu.first_name,
+      cu.last_name,
+      co.first_order_date,
       co.most_recent_order_date,
       coalesce(co.number_of_orders, 0) as number_of_orders
-      
   from customers cu
-
   left join customer_orders co using (customer_id)
-
 )
 
 select * from final order by customer_id asc
